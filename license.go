28e29ab229a0264125feb84ebf1322171043e72e
Jesse Hallam <jesse.hallam@gmail.com>
2020-09-08 11:05:30 -0300

    linting issues

--- license.go
+++ license.go
@@ -6,7 +6,7 @@ import (
 
 // IsEnterpriseLicensedOrDevelopment returns true when the server is licensed with any Mattermost
 // Enterprise License, or has `EnableDeveloper` and `EnableTesting` configuration settings
-// enabled signalling a non-production, developer mode.
+// enabled signaling a non-production, developer mode.
 func IsEnterpriseLicensedOrDevelopment(config *model.Config, license *model.License) bool {
 	if license != nil {
 		return true
@@ -17,9 +17,12 @@ func IsEnterpriseLicensedOrDevelopment(config *model.Config, license *model.Lice
 
 // IsE20LicensedOrDevelopment returns true when the server is licensed with a Mattermost
 // Enterprise E20 License, or has `EnableDeveloper` and `EnableTesting` configuration settings
-// enabled, signalling a non-production, developer mode.
+// enabled, signaling a non-production, developer mode.
 func IsE20LicensedOrDevelopment(config *model.Config, license *model.License) bool {
-	if license != nil && license.Features != nil && license.Features.FutureFeatures != nil && *license.Features.FutureFeatures {
+	if license != nil &&
+		license.Features != nil &&
+		license.Features.FutureFeatures != nil &&
+		*license.Features.FutureFeatures {
 		return true
 	}
 
@@ -27,10 +30,13 @@ func IsE20LicensedOrDevelopment(config *model.Config, license *model.License) bo
 }
 
 func isConfiguredForDevelopment(config *model.Config) bool {
-	if config != nil && config.ServiceSettings.EnableTesting != nil && *config.ServiceSettings.EnableTesting && config.ServiceSettings.EnableDeveloper != nil && *config.ServiceSettings.EnableDeveloper {
+	if config != nil &&
+		config.ServiceSettings.EnableTesting != nil &&
+		*config.ServiceSettings.EnableTesting &&
+		config.ServiceSettings.EnableDeveloper != nil &&
+		*config.ServiceSettings.EnableDeveloper {
 		return true
 	}
 
 	return false
-
 }
