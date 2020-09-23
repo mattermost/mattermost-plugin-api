4c0637afdcf1f3b555b78cd646c4b73c31e92246
Daniel Espino García <larkox@gmail.com>
2020-09-23 11:51:53 +0200

    Remove colons and substitute pronoun

--- experimental/telemetry/doc.go
+++ experimental/telemetry/doc.go
@@ -16,7 +16,7 @@
 // make sure the MM_RUDDER_DATA_PLANE_URL environment variable is set.
 //   LDFLAGS += -X "github.com/mattermost/mattermost-plugin-api/experimental/telemetry.rudderDataPlaneURL=$(MM_RUDDER_DATA_PLANE_URL)"
 //
-// In order to use it you should:
+// In order to use telemetry you should:
 // 1. Add the new fields to the plugin
 //   type Plugin struct {
 //     plugin.MattermostPlugin
@@ -24,7 +24,7 @@
 //     telemetryClient telemetry.Client
 //     tracker         telemetry.Tracker
 //   }
-// 2. Start the telemetry client on plugin activate:
+// 2. Start the telemetry client on plugin activate
 //  func (p *Plugin) OnActivate() error {
 //    p.telemetryClient, err = telemetry.NewRudderClient()
 //	  if err != nil {
@@ -38,7 +38,7 @@
 //      manifest.Version, "pluginName", enableDiagnostics, logger)
 //    return nil
 //  }
-// 4. Close the client on plugin deactivate:
+// 4. Close the client on plugin deactivate
 //  func (p *Plugin) OnDeactivate() error {
 //	  if p.telemetryClient != nil {
 //	    err := p.telemetryClient.Close()
