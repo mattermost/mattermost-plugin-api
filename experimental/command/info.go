6c0d120d40e08ef74ed0125df49856f046d6d6e5
Ben Schumacher <ben.schumacher@mattermost.com>
2022-11-01 10:14:12 +0100

    Revert "Export BuildInfo"
    This reverts commit 5bc49d0612d62d73b4b2163c782323dddec64801.

--- experimental/command/info.go
+++ experimental/command/info.go
@@ -11,70 +11,52 @@ import (
 	"github.com/pkg/errors"
 )
 
-type BuildInfo struct {
-	Manifest   model.Manifest
-	CommitSHA  string
-	BuildTime  time.Time
-	ModulePath string
-	GoVersion  string
-}
-
-func (b *BuildInfo) String() string {
-	path := b.ModulePath
-
-	matches := versionRegexp.FindAllString(path, -1)
-	if len(matches) > 0 {
-		path = strings.TrimSuffix(path, matches[len(matches)-1])
-	}
-
-	CommitSHAShort := b.CommitSHA[0:7]
-
-	commit := fmt.Sprintf("[%s](https://%s/commit/%s)", CommitSHAShort, path, b.CommitSHA)
-
-	return fmt.Sprintf("%s version: %s, %s, built %s with %s\n",
-		b.Manifest.Name,
-		b.Manifest.Version,
-		commit,
-		b.BuildTime.Format(time.RFC1123),
-		b.GoVersion)
-}
-
 var versionRegexp = regexp.MustCompile(`/v\d$`)
 
 func BuildInfoAutocomplete(cmd string) *model.AutocompleteData {
 	return model.NewAutocompleteData(cmd, "", "Display build info")
 }
 
-func GetBuildInfo(manifest model.Manifest) (*BuildInfo, error) {
+func BuildInfo(manifest model.Manifest) (string, error) {
 	info, ok := debug.ReadBuildInfo()
 	if !ok {
-		return nil, errors.New("failed to read build info")
+		return "", errors.New("failed to read build info")
 	}
 
 	var (
-		revision  string
-		buildTime time.Time
+		revision      string
+		revisionShort string
+		buildTime     time.Time
 	)
 	for _, s := range info.Settings {
 		switch s.Key {
 		case "vcs.revision":
 			revision = s.Value
-
+			revisionShort = revision[0:7]
 		case "vcs.time":
 			var err error
 			buildTime, err = time.Parse(time.RFC3339, s.Value)
 
 			if err != nil {
-				return nil, err
+				return "", err
 			}
 		}
 	}
 
-	return &BuildInfo{
-		Manifest:   manifest,
-		CommitSHA:  revision,
-		BuildTime:  buildTime,
-		ModulePath: info.Main.Path,
-		GoVersion:  info.GoVersion,
-	}, nil
+	path := info.Main.Path
+
+	matches := versionRegexp.FindAllString(path, -1)
+	if len(matches) > 0 {
+		path = strings.TrimSuffix(path, matches[len(matches)-1])
+	}
+
+	commit := fmt.Sprintf("[%s](https://%s/commit/%s)", revisionShort, path, revision)
+
+	return fmt.Sprintf("%s version: %s, %s, built %s with %s\n",
+			manifest.Name,
+			manifest.Version,
+			commit,
+			buildTime.Format(time.RFC1123),
+			info.GoVersion),
+		nil
 }
